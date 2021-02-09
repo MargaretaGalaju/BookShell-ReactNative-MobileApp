4e06bd4a2f8e8651985088addc3860fba6f6bf22
Margareta G <margareta.g@xor.ai>
2021-02-09 11:51:46 +0200

    Set project up, Added communication between 2 screens

---
+++ App.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import * as Font from 'expo-font';
+import Home from './screens/home';
+import AppLoading from 'expo-app-loading';
+import Navigator from './routes/homeStack';
+
+const getFonts = () => {
+  return Font.loadAsync({
+    'montserrat-regular': require('./assets/fonts/Montserrat-Regular.ttf'),
+    'montserrat-bold': require('./assets/fonts/Montserrat-Bold.ttf'),
+    'montserrat-italic': require('./assets/fonts/Montserrat-Italic.ttf'),
+    'montserrat-light': require('./assets/fonts/Montserrat-Light.ttf')
+  })
+}
+
+export default function App() {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  if (fontsLoaded) {
+    return (
+      <Navigator />
+    );
+  } else {
+    return (
+      <AppLoading
+        startAsync={getFonts}
+        onFinish={()=> setFontsLoaded(true)}
+        onError={()=> (
+            <View>
+                <Text>Error!</Text>
+            </View>
+        )}
+      />
+    )
+  }
+}
---
+++ screens/details.js
@@ -0,0 +1,125 @@
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View, TouchableOpacity, FlatList } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+
+export default function Details({ navigation }) {
+    const [hobbies, setHobbies] = useState([]);
+    const [inputValue, setInputValue] = useState('');
+
+    const onAddButtonClick = () => {
+        setHobbies((previousHobbies) => [
+            ...previousHobbies,
+            {text: inputValue, id: previousHobbies.length}
+        ]);
+        setInputValue('');
+    }
+
+    const onSave = () => {
+        navigation.navigate('Home', {
+            name: navigation.getParam('name'),
+            hobbies: hobbies,
+        })
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text>Hi, { navigation.getParam('name') }</Text>
+            <Text style={styles.text}>Hi! This is my second screen!</Text>
+            
+            <View style={styles.row}>
+                <TextInput
+                    onChangeText={inputValue => setInputValue(inputValue)}
+                    defaultValue={inputValue}
+                    style={styles.addInput}
+                    placeholder="Add your hobbies"
+                />
+                <TouchableOpacity onPress={onAddButtonClick} style={styles.appButtonContainer}>
+                    <Text style={styles.appButtonText}>Add</Text>
+                </TouchableOpacity>
+            </View>
+
+            <View style={styles.hobbies}>
+                <Text style={styles.text}>Your hobbies:</Text>
+                <Button title="Save Hobbies" onPress={onSave}/>
+
+                <FlatList 
+                    data={hobbies}
+                    renderItem={({item})=> (
+                        <View style={styles.hobby}>
+                            <Ionicons name="md-checkmark-circle"  />
+                            <Text style={styles.hobbyText}>{item.text}</Text>
+                        </View>
+                    )
+                    }
+                />
+            </View>
+        </View>
+    );
+  }
+
+  const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        paddingTop: 40,
+    },
+    button: {
+        margin: 20,
+        fontFamily: 'montserrat-regular',
+    },
+    row: {
+        display: 'flex',
+        flexDirection: 'row',
+    },
+    column: {
+        display: 'flex',
+        flexDirection: 'column',
+    },
+    hobbies: {
+        alignItems: 'flex-start',
+        display: 'flex',
+        marginTop: 20,
+    },
+    hobby: {
+        display: 'flex',
+        flexDirection: 'row',
+        marginBottom: 10,
+    },
+    hobbyText: {
+        marginLeft: 10,
+        fontFamily: 'montserrat-regular',
+    },
+    appButtonContainer: {
+        borderWidth:1,
+        borderTopRightRadius: 5,
+        borderBottomRightRadius: 5,
+        width: '20%',
+        backgroundColor: '#e1e1e1',
+        borderColor: '#e1e1e1',
+    },
+    addInput: {
+        borderWidth: 1,
+        borderTopLeftRadius: 5,
+        borderBottomLeftRadius: 5,
+        borderColor: '#e1e1e1',
+        width: '60%',
+        fontFamily: 'montserrat-regular',
+        padding: 20,
+    },
+    appButtonText: {
+        display: 'flex',
+        alignContent: 'center',
+        justifyContent: 'center',
+        alignSelf: 'center',
+        alignItems: 'center',
+        height: 60,
+        paddingTop: 20,
+        fontFamily: 'montserrat-bold',
+        color: "gray",
+    },
+   
+    text: {
+        fontFamily: 'montserrat-regular',
+        marginBottom: 20,
+    }
+  });
